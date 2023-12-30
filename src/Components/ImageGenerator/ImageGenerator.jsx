8a16732bdf9bd74dd24267d6d09a790307b44462
Sontara444 <sontararajput@gmail.com>
2023-12-30 22:38:24 +0530

    ai-image-generator

---
+++ src/Components/ImageGenerator/ImageGenerator.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import "./ImageGenerator.css"
+import default_image from '../Assets/default_image.svg'
+
+const ImageGenerator = () => {
+  return (
+    <div className='ai-image-generator'>
+        <div className="header">Ai Image <span>generator</span></div>
+        <div className="img-loading">
+            <div className="image">
+                <img src={default_image} alt="img" />
+            </div>
+        </div>
+        <div className="search-box">
+            <input type="text" className='search-input' placeholder='Describe What You Want To See ' />
+            <div className="generate-btn">
+                Generate
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ImageGenerator
